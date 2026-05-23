56c967e3c8574c21c5bc3d942867e6dd8559a5e7
JacksonEdwards-CSE <jacksoned0713@gmail.com>
2026-05-23 14:35:01 -0600

    Completed Lab 05

---
+++ sudoku_lab05.py
@@ -0,0 +1,216 @@
+# 1. Name:
+#      Jackson Edwards
+# 2. Assignment Name:
+#      Lab 05 : Sudoku Draft
+# 3. Assignment Description:
+#      Run the sudoku program and allow the user to input a value.
+# 4. What was the hardest part? Be as specific as possible.
+#      The hardest part for me was getting the board to display correctly. It wasn't
+#      too difficutlt, just weird to get everything to line up properly.
+# 5. How long did it take for you to complete the assignment?
+#      2 hours
+
+import json
+import os
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def read_file(filename):
+
+    with open(filename, "rt") as file:
+        json_data = file.read()
+
+        dictionary_data = json.loads(json_data)
+
+    return dictionary_data["board"]
+
+def save_file(filename, board):
+    try:
+        with open(filename, 'w') as filehandle:
+            json_data = json.dumps(board)
+            filehandle.write("{\"board\" : \n"+ json_data + "\n}")
+
+    except (FileExistsError, FileNotFoundError):
+        print("File not found")
+
+def display_board(board):
+    print()
+    print("   A B C D E F G H I")
+    for row in range(9):
+        print(row + 1, end="  ")
+        for column in range(9):
+
+            if board[row][column] == 0:
+                print(" ", end="")
+            
+            else:
+                print(f"{board[row][column]}", end="")
+
+            if column % 3 == 2 and column != 8:
+                print("|", end="")
+
+            elif column == 8:
+                print()
+            
+            else:
+                print(end=" ")
+
+        if row % 3 == 2 and row != 8: 
+            print("   -----+-----+-----")
+
+def check_user_square(square, board):
+
+    column_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+
+    column = 0
+    row = 0
+
+    if len(square) > 2 or len(square) < 2:
+        print("Invalid Input")
+        return row, column, False
+    
+    elif square[0] in column_list and 0 < int(square[1]) < 10:
+
+        column = column_list.index(square[0])
+
+        row = int(square[1]) - 1
+    
+    elif square[1] in column_list and 0 < int(square[0]) < 10:
+
+        column = column_list.index(square[1])
+
+        row = int(square[0]) - 1
+    
+    else:
+        print("Invalid Input")
+        return row, column, False
+
+    if check_square_availability(row, column, board):
+        return row, column, True
+    else:
+        print("Square Already Filled")
+
+        return row, column, False
+
+def check_square_availability(row, column, board):
+
+    if board[row][column] == 0:
+        return True
+    else:
+        return False
+
+def check_user_number(row, column, number, board):
+
+    #Checking if number is in 1-9:
+
+    if 0 > number or number > 9:
+        print("Invalid Number\n")
+
+        return False
+
+    #Checking rows and columns:
+
+    if number in board[row]:
+        print(f"{number} is already in that row\n")
+        return False
+    
+    for i in range(9):
+
+        if number == board[i][column]:
+            print(f"{number} is already in that column\n")
+            return False
+    
+    #Checking 3x3 square:
+
+    box_left_column = column - (column % 3)
+
+    box_top_row = row - (row % 3)
+
+    for r in range(box_top_row, box_top_row + 3):
+
+        for c in range(box_left_column, box_left_column + 3):
+
+            if number == board[r][c]:
+                print(f"{number} is already in that box\n")
+                return False
+    
+    return True
+    
+def edit_board(row, column, number, board):
+
+    board[row][column] = number
+
+def check_win(board):
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == 0:
+                return False
+    
+    return True
+
+def main():
+    
+    filename = input("Please enter a filename: ")
+
+    board = read_file(filename)
+
+    play_number = 0
+    i = 1
+
+    done = False
+
+    while not done:
+
+        play_number += 1
+
+        valid_square = False
+
+        while not valid_square:
+            display_board(board)
+
+            print("Specify a coordinate to edit or 'Q' to save and quit")
+            square = input("> ").lower()
+
+            if square == "q":
+                filename = input("Where would you like to save your game? (include .json) ")
+                save_file(filename, board)
+                return
+            
+            else:
+                row, column, valid_square = check_user_square(square, board)
+
+        valid_number = False
+
+        while not valid_number:  
+            number = int(input(f"What number goes in {square}? "))
+
+            if square == "q":
+                return
+            
+            else:
+                valid_number = check_user_number(row, column, number, board)
+        
+        edit_board(row, column, number, board)
+
+        done = check_win(board)
+
+        if play_number % 5 == 0:
+            save_file(f"{filename}.InProgress.{i}.json", board)
+
+            print(f"Board Autosaved to {filename}.InProgress.{i}.json")
+            i+=1
+
+    clear_console()
+
+    display_board(board)
+
+    print("Congrats, you completed the puzzle!")
+
+    save_file(f"{filename}.Completed.json", board)
+
+    print(f"Board Autosaved to {filename}.Completed.json")
+
+if __name__ == "__main__":
+    main()
